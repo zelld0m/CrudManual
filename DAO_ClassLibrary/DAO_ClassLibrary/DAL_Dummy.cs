8c5dd5d55a9eaaf0339c06438480e3260eae0722
Aizel Nunag <izelllim@gmail.com>
2016-11-11 11:13:57 +0800

    creating DAL using Stored Procedure of Database

--- DAO_ClassLibrary/DAO_ClassLibrary/DAL_Dummy.cs
+++ DAO_ClassLibrary/DAO_ClassLibrary/DAL_Dummy.cs
@@ -15,84 +15,97 @@ namespace DAO_ClassLibrary
 
 
         SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
-        public void AddDummy(String Name)
+        #region xx
+
+        //public void AddDummy(String Name)
+        //{
+        //    SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
+
+        //    con.Open();
+        //    SqlCommand cmd = con.CreateCommand();
+        //    cmd.CommandText = "INSERT INTO dummy VALUES ('" + Name + "')";
+        //    cmd.ExecuteNonQuery();
+        //    con.Close();
+        //}
+        //public void DeleteDummy(int Delete_ID)
+        //{
+        //    SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
+
+        //    con.Open();
+        //    SqlCommand cmd = con.CreateCommand();
+        //    cmd.CommandText = "DELETE  FROM dummy WHERE ID = " + Delete_ID + "";
+        //    cmd.ExecuteNonQuery();
+        //    con.Close();
+        //}
+        //public DataTable ViewDummy()
+        //{
+        //    //  SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
+
+        //    con.Open();
+        //    SqlCommand cmd = con.CreateCommand();
+        //    cmd.CommandText = "SELECT * FROM Dummy";
+        //    cmd.ExecuteNonQuery();
+        //    DataTable dt = new DataTable();
+        //    SqlDataAdapter da = new SqlDataAdapter(cmd);
+        //    da.Fill(dt);
+        //    // datagridview.datasource = dt;
+        //    con.Close();
+        //    return dt;   // use  dt DataTable as datasource
+
+        //}
+        //public DataTable SearchDummy(String inputName)
+        //{
+
+        //    SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
+
+        //    con.Open();
+        //    SqlCommand cmd = con.CreateCommand();
+        //    cmd.CommandText = "SELECT * FROM Dummy Where Name = '" + inputName + "'";
+        //    cmd.ExecuteNonQuery();
+        //    DataTable dt = new DataTable();
+        //    SqlDataAdapter da = new SqlDataAdapter(cmd);
+        //    da.Fill(dt);
+        //    // datagridview.datasource = dt;
+        //    con.Close();
+        //    return dt;   // use  dt DataTable as datasource
+
+        //}
+
+        //public void UpdateDummy(int inputID, String Name)
+        //{
+        //    SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
+
+        //    con.Open();
+        //    SqlCommand cmd = con.CreateCommand();
+        //    cmd.CommandText = "UPDATE dummy Set Name = '" + Name + "' WHERE ID = " + inputID + "";
+        //    cmd.ExecuteNonQuery();
+        //    con.Close();
+        //}
+        #endregion
+        public DataSet ViewALL( )
         {
-            SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
 
-            con.Open();
             SqlCommand cmd = con.CreateCommand();
-            cmd.CommandText = "INSERT INTO dummy VALUES ('" + Name + "')";
-            cmd.ExecuteNonQuery();
-            con.Close();
+            //cmd.CommandText = "Select * from dummy";
+            SqlDataAdapter sda = new SqlDataAdapter("SelectALL_Dummy",con);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+           
+            return ds ;
         }
-        public void DeleteDummy(int Delete_ID)
-        {
-            SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
-
-            con.Open();
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandText = "DELETE  FROM dummy WHERE ID = " + Delete_ID + "";
-            cmd.ExecuteNonQuery();
-            con.Close();
-        }
-        public DataTable ViewDummy()
-        {
-            //  SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
-
-            con.Open();
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandText = "SELECT * FROM Dummy";
-            cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
-            // datagridview.datasource = dt;
-            con.Close();
-            return dt;   // use  dt DataTable as datasource
-
-        }
-        public DataTable SearchDummy(String inputName)
-        {
-
-            SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
-
-            con.Open();
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandText = "SELECT * FROM Dummy Where Name = '" + inputName + "'";
-            cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
-            // datagridview.datasource = dt;
-            con.Close();
-            return dt;   // use  dt DataTable as datasource
-
-        }
-
-        public void UpdateDummy(int inputID, String Name)
-        {
-            SqlConnection con = new SqlConnection("Data Source=DMNLANUNAG;Initial Catalog=Registration;Integrated Security=True");
-
-            con.Open();
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandText = "UPDATE dummy Set Name = '" + Name + "' WHERE ID = " + inputID + "";
-            cmd.ExecuteNonQuery();
-            con.Close();
-        }
-
-        public SqlDataAdapter viewAllxx(DataTable dtx )
+        public DataSet insert_Dummy(SqlCommand cmd)
         {
             con.Open();
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandText = "SELECT * FROM Dummy";
-            cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-    
-            da.Fill(dt);
-            return da;
-        }
+            cmd.Connection = con;
+            cmd.CommandText = "Insert_Dummy"
 
+
+            SqlDataAdapter sda = new SqlDataAdapter("SelectALL_Dummy", con);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+
+            return ds;
+        }
 
     }
 }
