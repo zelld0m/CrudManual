7eef30882fb356bea755c82b7b8bcf13c27d12e1
Aizel Nunag <izelllim@gmail.com>
2016-11-22 11:10:28 +0800

    console added, test for xml app  for reader=

---
+++ DAO_ClassLibrary/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sample();
+        }
+
+
+        static void sample()
+        {
+
+            //XmlTextReader reader = new XmlTextReader("Books.xml");
+            XmlTextReader reader = new XmlTextReader("EDP.xml");
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+                        Console.Write("<" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                    case XmlNodeType.Text: //Display the text in each element.
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+            Console.ReadLine();
+       
+        }
+    }
+}
