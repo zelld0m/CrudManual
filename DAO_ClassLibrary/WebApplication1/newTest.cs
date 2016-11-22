006ae934557007e6f36c888d7416433d4eef683a
Aizel Nunag <izelllim@gmail.com>
2016-11-22 13:37:32 +0800

    generated Serializer by using Xsd2CodeGenerator that was Installed Yesterday

---
+++ DAO_ClassLibrary/WebApplication1/newTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace WebApplication1
+{
+    public class newTest
+    {
+
+
+
+
+
+
+        public void test()
+        {
+       
+            
+        }
+    }
+
+
+
+}
