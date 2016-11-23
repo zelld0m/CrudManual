9b04ba1916e10342c812bf72d79a8a2130df38cd
Aizel Nunag <izelllim@gmail.com>
2016-11-23 11:25:50 +0800

    Test2 Complete. Creating new project for usage of XSD2 Code Generator

--- DAO_ClassLibrary/Test2/Program.cs
+++ DAO_ClassLibrary/Test2/Program.cs
@@ -12,26 +12,44 @@ namespace Test2
     {
         static void Main(string[] args)
         {
-            #region De _ Serialization
+            #region LIST _ Serialization
 
+            //LIST PRODUCT SERIALIZATION
 
-            //product Serialization
             try
             {
-                Product product = new Product
+                List<Product> listProduct = new List<Product>();
+                listProduct.Add(new Product
                 {
                     ID = "p01",
                     Name = "product Name 1",
                     CategoryName = "category 1",
                     price = new Price { Value = 100, Unit = "USD" },
                     description = new Description { Color = "RED", Size = "Size dd1", Weight = "100gr" }
-                };
-            //--------ERROR 
-                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Product));
-              
-                StreamWriter sw = new StreamWriter("Product.xml");
+                });
+                listProduct.Add(new Product
+                {
+                    ID = "p02",
+                    Name = "product Name 2",
+                    CategoryName = "category 2",
+                    price = new Price { Value = 100, Unit = "AUD" },
+                    description = new Description { Color = "BLUE", Size = "Size 2", Weight = "200gr" }
+                });
 
-                xmlSerializer.Serialize(sw, product);
+                listProduct.Add(new Product
+                {
+                    ID = "p03",
+                    Name = "product Name 3",
+                    CategoryName = "category 3",
+                    price = new Price { Value = 100, Unit = "EUR" },
+                    description = new Description { Color = "yellow", Size = "Size 3", Weight = "300gr" }
+                });
+                //--------ERROR 
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Product>));
+
+                StreamWriter sw = new StreamWriter("listProduct.xml");
+
+                xmlSerializer.Serialize(sw, listProduct);
                 sw.Close();
                 //ERROR
                 Console.WriteLine("Serialization Success");
@@ -40,8 +58,61 @@ namespace Test2
             {
                 Console.WriteLine(ex.Message);
             }
+            /// LIST Desirialization
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Product));  // FORMATTER
+                StreamReader sr = new StreamReader("listProduct.xml");     // GET FROM
+                List<Product> listProduct = (List<Product>)xmlSerializer.Deserialize(sr);     // DESERIALIZE PRODUCTS
+                foreach (Product product in listProduct) // SHOW ALL PRODUCTS
+                {
+                    Console.WriteLine("list Product Information");
+                    Console.WriteLine("ID: " + product.ID);
+                    Console.WriteLine("NAME: " + product.Name);
+                    Console.WriteLine("Price: " + product.price.Value);
+                    Console.WriteLine("unit: " + product.price.Unit);
+                    Console.WriteLine("Color: " + product.description.Color);
+                    Console.WriteLine("Size: " + product.description.Size);
+                    Console.WriteLine("Weight: " + product.description.Weight);
+                    Console.Write("__________________________________________");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
+            #endregion LIST _ Serialization
+
+            //product Serialization
+            /*   try
+               {
+                   Product product = new Product
+                   {
+                       ID = "p01",
+                       Name = "product Name 1",
+                       CategoryName = "category 1",
+                       price = new Price { Value = 100, Unit = "USD" },
+                       description = new Description { Color = "RED", Size = "Size dd1", Weight = "100gr" }
+                   };
+               //--------ERROR 
+                   XmlSerializer xmlSerializer = new XmlSerializer(typeof(Product));
+
+                   StreamWriter sw = new StreamWriter("Product.xml");
+
+                   xmlSerializer.Serialize(sw, product);
+                   sw.Close();
+                   //ERROR
+                   Console.WriteLine("Serialization Success");
+               }
+               catch (Exception ex)
+               {
+                   Console.WriteLine(ex.Message);
+               }
+               */
             //Product Desirialization
+
+            /*
             try
             {
                 XmlSerializer xmlSerializer = new XmlSerializer(typeof(Product));
@@ -53,6 +124,8 @@ namespace Test2
                 Console.WriteLine("Price: " + Product.price.Value);
                 Console.WriteLine("unit: " + Product.price.Unit);
                 Console.WriteLine("Color: " + Product.description.Color);
+                Console.WriteLine("Size: " + Product.description.Size);
+                Console.WriteLine("Weight: " + Product.description.Weight);
             }
             catch (Exception ex)
             {
@@ -60,6 +133,7 @@ namespace Test2
             }
             #endregion De _ Serialization
             Console.ReadLine();
+            */
         }
     }
 }
